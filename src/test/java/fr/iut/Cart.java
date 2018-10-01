2b04603878f9c859bf02c20044a47d1fb81344d8
Clement Agarini <716892+imclem@users.noreply.github.com>
2018-10-01 13:55:55 +0200

    Add capability to compute total price without taxes

---
+++ src/test/java/fr/iut/Cart.java
@@ -0,0 +1,28 @@
+package fr.iut;
+
+import fr.ca.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private final Map<Item, Integer> products = new HashMap<Item, Integer>();
+
+    public void addProduct(final Item item, final int quantity) {
+        products.put(item, quantity);
+    }
+
+    public int countProducts() {
+        return products.keySet().size();
+    }
+
+    public double computePrice() {
+        double totalPrice = 0.0d;
+
+        for (Item i : products.keySet()) {
+            totalPrice += (i.getPrice() * products.get(i));
+        }
+
+        return totalPrice;
+    }
+}
